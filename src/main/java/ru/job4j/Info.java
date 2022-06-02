d53494b125c39c666fd591b2cd665054062ef303
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-06-02 17:04:38 +0700

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,8 +1,7 @@
 package ru.job4j;
 
 public class Info {
-    public static class  info {
         public static void main(String[] args) {
             System.out.println("02.06.2022");
         }
-    }}
+    }
