d4ef23bdc827506261effc1f32ab24c317257fe7
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-06-02 11:24:32 +0700

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ivan Pomarkov");
+        System.out.println("14.02.2002");
+    }
+}
