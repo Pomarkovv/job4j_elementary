2844e522df42ff8188f946d776e71fd1e1afa9f4
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-06-06 19:23:58 +0700

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String space = " ";
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += space + "But i am a newbie";
+        int year = 2022;
+        idea += space + year;
+        System.out.println(idea);
+    }
+}
