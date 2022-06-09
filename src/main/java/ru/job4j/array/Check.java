246cfbc8b8e66ae8ec7a49fd631b18ee6366297e
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-06-09 19:07:50 +0700

    Исправление в классе Check

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -3,10 +3,11 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 1; i < data.length; i++) {
             boolean first = data[0];
             if (data[i] != first) {
                 result = false;
+                break;
             }
         }
         return result;
