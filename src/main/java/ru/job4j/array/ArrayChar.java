8c6818fedb280b18558edb04f5f458bf282cfc58
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-06-09 12:40:42 +0700

    Исправление в классе FindLoop

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+            for (int i = 0; i < word.length; i++) {
+                for (int j = 0; j < pref.length; j++) {
+                    if (word[i] != pref[j]) {
+                        result = false;
+                    }
+                } }
+        return result;
+
+} }
