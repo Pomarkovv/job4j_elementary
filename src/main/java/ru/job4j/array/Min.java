6ece2f8fc71e0214956af3cf6fcac4e60e852f96
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-06-11 07:28:10 +0700

    Изменения в классе Min

--- src/main/java/ru/job4j/array/Min.java
+++ src/main/java/ru/job4j/array/Min.java
@@ -4,7 +4,7 @@ package ru.job4j.array;
         public static int findMin(int[] array) {
             int min = array[0];
             for (int index = 1; index < array.length; index++) {
-                if (array[index] <= min) {
+                if (array[index] < min) {
                     min = array[index];
                 }
             }
